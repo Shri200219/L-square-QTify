bd56375ffbb104b82231fe8277e5a1b8137e2319
Shriram Prajapati <shriramprajapati@Shrirams-MacBook-Air.local>
2023-12-29 11:02:26 +0530

    Qtify

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Search from "../Search/Search";
+import Button from "../Button/Button";
+import styles from "./Navbar.module.css";
+import Logo from "./../Logo/Logo"
+import { Link } from "react-router-dom";
+
+function Navbar({ searchData }) {
+    return (
+        <nav className={styles.navbar}>
+            <Link href="/"> <Logo/>
+            </Link>
+            <Search
+                placeholder={"Search a song of your choice"}
+                searchData={searchData}
+            />
+            <Button>Give Feedback</Button>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ qtify/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+import Hero from "../../components/Hero/Hero";
+import Section from "../../components/Section/Section";
+import { useOutletContext } from "react-router-dom";
+import { fetchFilters } from "../../api/api"; 
+
+function HomePage() {
+    const { data } = useOutletContext();
+    const { topAlbums, newAlbums, songs } = data;
+    return (
+        <>
+            <Hero/>
+            <div className={styles.wrapper}>
+                <Section title="Top Albums" data={topAlbums} type="album" />
+                <Section title="New Albums" data={newAlbums} type="album" />
+                <Section
+                    title="Songs"
+                    data={songs}
+                    filterSource={fetchFilters}
+                    type="song" />
+            </div>
+
+        </>
+    )
+
+
+}
+export default HomePage;
